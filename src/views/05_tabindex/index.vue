d2a0980f7408f54c7ad55bf263bd8a28b5d3be1f
zhaosuizhen <1026688629@qq.com>
2020-05-06 10:24:02 +0800

    地图05的详细注释

--- src/views/05_tabindex/index.vue
+++ src/views/05_tabindex/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="vm">
     <h2 class="h-title">点击激活地图 tabindex</h2>
-    <div tabindex="2" id="map" class="map-x"></div>
+
+    <!-- 在这里设置'tabindex="1"'可实现选中激活 -->
+    <div tabindex="1" id="map" class="map-x"></div>
+
     <div class="explain">
-      <p>通过给地图容器设置 tabindex="2" 来控制默认不会选中地图。</p>
+      <p>通过给地图容器设置 tabindex="1" 来控制默认不会选中地图。</p>
       <p>只有当鼠标点击地图时，才能进行缩放等操作。</p>
       <p>如果没选中地图，即使在地图上滑动滚轮，也不会操作到地图</p>
     </div>
-    <div class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, earum voluptatum. Culpa consequatur reiciendis ea iure quis voluptatibus dolore laborum perferendis inventore, eaque harum quisquam earum recusandae nesciunt, expedita incidunt iste. Facilis fuga ut neque deserunt omnis magni aperiam officiis. Architecto officiis qui eius, facere facilis quis, omnis placeat, autem excepturi tempora eveniet culpa beatae consectetur repellendus tenetur ad molestias explicabo veniam blanditiis? Amet consectetur necessitatibus obcaecati, praesentium possimus sunt dolore magnam nostrum quidem atque minima fugiat quaerat recusandae nisi?</div>
   </div>
 </template>
 
@@ -28,11 +30,13 @@ export default {
     initMap () {
       this.map = new Map({
         target: "map",
+
         layers: [
           new Tile({
             source: new OSM()
           })
         ],
+
         view: new View({
           projection: "EPSG:4326",
           center: [113.120444,23.034742],
@@ -51,5 +55,8 @@ export default {
   .content {
     width: 100px;
     margin: 10px auto;
+  }
+  .explain{
+    height: 200vh;
   }
 </style>
